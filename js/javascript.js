61c96115193b40ad6bf4e204da0aabf359a60bf3
Baloo21 <william.brody15@gmail.com>
2021-05-14 15:41:38 -0400

    jquery

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,11 @@
-
-
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
+}
 
 
 
